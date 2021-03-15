21e549f501d478aeff72bb2d27e43ddf05817c1d
Anh Vu <vu.haianh291@gmail.com>
2021-03-15 15:25:52 +0200

    Implement logic for user controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,25 +1,104 @@
-exports.getUsers = () => {};
+const mongoose = require('mongoose')
+const User = mongoose.model('User')
 
-exports.getAuthUser = () => {};
+exports.getUsers = async (req, res, next) => {
+    const users = await User.find().select('_id name email createdAt updatedAt')
+    res.json(users)
+}
 
-exports.getUserById = () => {};
+exports.getAuthUser = async (req, res,) => {
+    if (!req.isAuthUser) {
+        res.status(403).json({
+            message: 'You are unauthenticated'
+        })
+        return res.redirect('/signin')
+    } else {
+        return res.json(req.user)
+    }
 
-exports.getUserProfile = () => {};
+}
 
-exports.getUserFeed = () => {};
+exports.getUserById = async (req, res, next, id) => {
+    req.profile = await User.findOne({_id: id})
 
-exports.uploadAvatar = () => {};
+    const profileId = mongoose.Types.ObjectId(req.profile._id)
+    if (profileId.equals(req.user._id)) {
+        req.isAuthUser = true
+        return next()
+    }
+    next()
+}
 
-exports.resizeAvatar = () => {};
+exports.getUserProfile = async (req, res) => {
+    if (!req.profile) {
+        return res.status(404).json({
+            message: 'No user found'
+        })
+    }
+    res.json(req.profile)
+};
 
-exports.updateUser = () => {};
+exports.getUserFeed = () => {
+};
 
-exports.deleteUser = () => {};
+exports.uploadAvatar = () => {
+};
 
-exports.addFollowing = () => {};
+exports.resizeAvatar = () => {
+};
 
-exports.addFollower = () => {};
+exports.updateUser = () => {
+};
 
-exports.deleteFollowing = () => {};
+exports.deleteUser = async (req, res) => {
+    const {userId} = req.params
+    if (req.isAuthUser) {
+        await User.findOneAndDelete({_id: userId})
+        return res.status(204)
+    }
+    return res.status(403).message(`Forbidden error`)
+}
 
-exports.deleteFollower = () => {};
+exports.addFollowing = async (req, res, next) => {
+    const {followId} = req.body
+    await User.findByIdAndUpdate({
+        _id: req.user.id,
+    }, {
+        $push: {following: followId}
+    })
+    next()
+}
+
+exports.addFollower = async (req, res, next) => {
+    const {followId} = req.body
+    const user = await User.findByIdAndUpdate({
+        _id: followId,
+    }, {
+        $push: {followers: followId}
+    }, {
+        new: true
+    })
+    return res.json({user})
+};
+
+exports.deleteFollowing = async (req, res, next) => {
+    const {followId} = req.body
+    await User.findByIdAndUpdate({
+        _id: req.user.id,
+    }, {
+        $pull: {following: followId}
+    })
+    next()
+}
+
+exports.deleteFollower = async (req, res, next) => {
+    const {followId} = req.body
+    const user = await User.findByIdAndUpdate({
+        _id: followId,
+    }, {
+        $pull: {followers: followId}
+    }, {
+        new: true
+    })
+    return res.json({user})
+}
