2c77513aab362d2667bde37fa0ab4536b99f33fd
Anh Vu <vu.haianh291@gmail.com>
2021-03-16 12:17:11 +0200

    Feat: Sign up form

--- pages/signup.js
+++ pages/signup.js
@@ -1,26 +1,77 @@
-// import Typography from "@material-ui/core/Typography";
-// import Avatar from "@material-ui/core/Avatar";
-// import FormControl from "@material-ui/core/FormControl";
-// import Paper from "@material-ui/core/Paper";
-// import Input from "@material-ui/core/Input";
-// import InputLabel from "@material-ui/core/InputLabel";
-// import Button from "@material-ui/core/Button";
-// import Snackbar from "@material-ui/core/Snackbar";
-// import Dialog from "@material-ui/core/Dialog";
-// import DialogActions from "@material-ui/core/DialogActions";
-// import DialogContent from "@material-ui/core/DialogContent";
-// import DialogContentText from "@material-ui/core/DialogContentText";
-// import DialogTitle from "@material-ui/core/DialogTitle";
-// import Slide from "@material-ui/core/Slide";
-// import Gavel from "@material-ui/icons/Gavel";
-// import VerifiedUserTwoTone from "@material-ui/icons/VerifiedUserTwoTone";
+import React from 'react'
+import Typography from "@material-ui/core/Typography";
+import Avatar from "@material-ui/core/Avatar";
+import FormControl from "@material-ui/core/FormControl";
+import Paper from "@material-ui/core/Paper";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
+import Button from "@material-ui/core/Button";
+import Snackbar from "@material-ui/core/Snackbar";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Slide from "@material-ui/core/Slide";
+import Gavel from "@material-ui/icons/Gavel";
+import VerifiedUserTwoTone from "@material-ui/icons/VerifiedUserTwoTone";
 import withStyles from "@material-ui/core/styles/withStyles";
+import {signupUser} from "../lib/auth";
 
 class Signup extends React.Component {
-  state = {};
+  state = {
+    email: '',
+      name:'',
+      password: ''
+  }
 
+  handleChange = (e) => {
+    this.setState({
+        [e.target.name]: e.target.value
+    })
+  }
+
+  handleSubmit = async e => {
+    e.preventDefault()
+      const {name, email, password} = this.state
+      const user = {
+      name,
+          email,
+          password
+      }
+      const data = await signupUser(user)
+  }
   render() {
-    return <div>Signup</div>;
+    const {classes} = this.props
+    return (
+        <div className={classes.root}>
+          <Paper className={classes.paper}>
+            <Avatar className={classes.avatar}>
+              <Gavel/>
+            </Avatar>
+            <Typography variant='h5' component='h1'>
+              Sign up
+            </Typography>
+            <form className={classes.form} onSubmit={this.handleSubmit}>
+              <FormControl margin='normal' required fullWidth>
+                <InputLabel htmlFor='name'>Name</InputLabel>
+                <Input name='name' type='text' onChange={this.handleChange}/>
+              </FormControl>
+                <FormControl margin='normal' required fullWidth>
+                    <InputLabel htmlFor='email'>Email</InputLabel>
+                    <Input name='email' type='text' onChange={this.handleChange}/>
+                </FormControl>
+                <FormControl margin='normal' required fullWidth>
+                    <InputLabel htmlFor='password'>Password</InputLabel>
+                    <Input name='password' type='password' onChange={this.handleChange}/>
+                </FormControl>
+                <Button type='submit' fullWidth variant='contained' color='primary'>
+                  Sign up
+                </Button>
+            </form>
+          </Paper>
+        </div>
+    )
   }
 }
 
